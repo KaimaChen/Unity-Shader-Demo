4b60bbc86d138bfedea90062df66593f83967716
Kaima <244241917@qq.com>
2017-08-07 01:40:23 +0800

    消融 - 场景消融

---
+++ UnityShaderProject/Assets/Dissolve/Scripts/CombineMeshes.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombineMeshes : MonoBehaviour {
+    void Awake()
+    {
+        Combine();
+    }
+
+    void Combine()
+    {
+        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();       //获取自身和所有子物体中所有MeshFilter组件
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length];    //新建CombineInstance数组
+
+        for (int i = 0; i < meshFilters.Length; i++)
+        {
+            combine[i].mesh = meshFilters[i].sharedMesh;
+            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            meshFilters[i].gameObject.SetActive(false);
+        }
+
+        transform.GetComponent<MeshFilter>().mesh = new Mesh();
+        transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);       //合并
+        transform.gameObject.SetActive(true);
+    }
+}
